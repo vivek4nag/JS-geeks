b0a7985d64b42e75e07a31dd9e4f0e2b3b9a2e3b
viek4nag <vivek4nag@gmail.com>
2024-10-23 22:04:32 +0530

    basic dom manipulation

---
+++ 06_DOM_project_1/script.js
@@ -0,0 +1,11 @@
+const box = document.getElementById("dabba")
+
+box.style.backgroundColor = "yellow";
+box.style.margin = "20px"
+box.style.padding = "10px"
+box.style.fontSize = "18px"
+box.style.fontWeight = "bold"
+box.style.height = "200px"
+box.style.width = "300px"
+box.innerText = "all styling given using JS"
+box.style.color = "red"
